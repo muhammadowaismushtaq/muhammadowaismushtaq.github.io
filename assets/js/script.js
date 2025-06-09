610b8673d7d42bb19a4c9e130b65d9025904cc4f
Muhammad Owais Mushtaq <owais.mushtaq@softpak.com>
2025-06-09 14:10:31 +0500

    Added Form Submission functionality and add resume

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,9 @@
  * element toggle function
  */
 
-const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
+const elemToggleFunc = function (elem) {
+    elem.classList.toggle("active");
+}
 
 
 /**
@@ -17,16 +18,15 @@ const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
 
-  if (window.scrollY >= 10) {
-    header.classList.add("active");
-    goTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    goTopBtn.classList.remove("active");
-  }
+    if (window.scrollY >= 10) {
+        header.classList.add("active");
+        goTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        goTopBtn.classList.remove("active");
+    }
 
 });
-
 
 
 /**
@@ -38,12 +38,11 @@ const navbar = document.querySelector("[data-navbar]");
 
 navToggleBtn.addEventListener("click", function () {
 
-  elemToggleFunc(navToggleBtn);
-  elemToggleFunc(navbar);
-  elemToggleFunc(document.body);
+    elemToggleFunc(navToggleBtn);
+    elemToggleFunc(navbar);
+    elemToggleFunc(document.body);
 
 });
-
 
 
 /**
@@ -55,15 +54,16 @@ const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
 const skillsBox = document.querySelector("[data-skills-box]");
 
 for (let i = 0; i < toggleBtns.length; i++) {
-  toggleBtns[i].addEventListener("click", function () {
+    toggleBtns[i].addEventListener("click", function () {
 
-    elemToggleFunc(toggleBtnBox);
-    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
-    elemToggleFunc(skillsBox);
+        elemToggleFunc(toggleBtnBox);
+        for (let i = 0; i < toggleBtns.length; i++) {
+            elemToggleFunc(toggleBtns[i]);
+        }
+        elemToggleFunc(skillsBox);
 
-  });
+    });
 }
-
 
 
 /**
@@ -74,19 +74,19 @@ const themeToggleBtn = document.querySelector("[data-theme-btn]");
 
 themeToggleBtn.addEventListener("click", function () {
 
-  elemToggleFunc(themeToggleBtn);
+    elemToggleFunc(themeToggleBtn);
 
-  if (themeToggleBtn.classList.contains("active")) {
-    document.body.classList.remove("dark_theme");
-    document.body.classList.add("light_theme");
+    if (themeToggleBtn.classList.contains("active")) {
+        document.body.classList.remove("dark_theme");
+        document.body.classList.add("light_theme");
 
-    localStorage.setItem("theme", "light_theme");
-  } else {
-    document.body.classList.add("dark_theme");
-    document.body.classList.remove("light_theme");
+        localStorage.setItem("theme", "light_theme");
+    } else {
+        document.body.classList.add("dark_theme");
+        document.body.classList.remove("light_theme");
 
-    localStorage.setItem("theme", "dark_theme");
-  }
+        localStorage.setItem("theme", "dark_theme");
+    }
 
 });
 
@@ -95,11 +95,11 @@ themeToggleBtn.addEventListener("click", function () {
  */
 
 if (localStorage.getItem("theme") === "light_theme") {
-  themeToggleBtn.classList.add("active");
-  document.body.classList.remove("dark_theme");
-  document.body.classList.add("light_theme");
+    themeToggleBtn.classList.add("active");
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
 } else {
-  themeToggleBtn.classList.remove("active");
-  document.body.classList.remove("light_theme");
-  document.body.classList.add("dark_theme");
+    themeToggleBtn.classList.remove("active");
+    document.body.classList.remove("light_theme");
+    document.body.classList.add("dark_theme");
 }
